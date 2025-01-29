07baa651f9773eef1c504f841620415f74537b6a
Simone Braga <simone.braga@engineer.com>
2025-01-29 16:49:19 +0100

    Update formula: kubectl-cnpg

--- Formula/kubectl-cnpg.rb
+++ Formula/kubectl-cnpg.rb
@@ -2,20 +2,20 @@ class KubectlCnpg < Formula
   desc "CloudNativePG plugin for kubectl"
   homepage "https://cloudnative-pg.io/"
   url "https://github.com/cloudnative-pg/cloudnative-pg.git",
-      tag:      "v1.23.1",
-      revision: "336ddf5308fe0d5cf78c4da1d03959fa02a60c70"
+      tag:      "v1.24.0",
+      revision: "5fe5bb6b9292c73ebfc1463680b8863abbdb3de3"
   license "Apache-2.0"
   head "https://github.com/cloudnative-pg/cloudnative-pg.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "2a0bc1f7c973eee09662a1a1935e95e83dd55129a8f92e9c9eb8a0474feb618a"
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "e4c82a28026cadd104395dda232302940b47e05d8bd8419a38ea77f9f6f0b70e"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "098baeb0085db072740805d58566b8002fc03f155f9982d4e7a613b35a0f888e"
-    sha256 cellar: :any_skip_relocation, sonoma:         "6f90320184cc066cae8a42d32695dff1779edc7f2fd0d5c609f7166e3ea01cf4"
-    sha256 cellar: :any_skip_relocation, ventura:        "5fa7175b9234d3354305df2e65cc493ee4c5b3e2480fbec68eeba99f1cddc344"
-    sha256 cellar: :any_skip_relocation, monterey:       "f7604027cda0bf89ec97e0e0039a19eb7a9e408355ec6a3ea225d054fbb45423"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "599802b5d9c2b751573c423e2121aa0f43f09a1f55200f7d69c3e11e67de26cf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia:  "a2fa094354e54a0eb5f2afcc24d9f98ac3a7cd3e2662b96997b36f7d7ab99371"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:   "08194de6003590e2995aeb969b9973fa9bdeb0d549f4c44055a20169145eca16"
+    sha256 cellar: :any_skip_relocation, arm64_ventura:  "fca31f527336ae2f92871afc5bb31618b61e4c14b9c8e131b015a1bb092e6576"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "3976abe30219354b7703b43abdeff8c8b9b132fa7bd741bca8e88797121f1419"
+    sha256 cellar: :any_skip_relocation, sonoma:         "93dc44ab70491f01be212aec223dea392b77e392dda855bc96ca0d9c58c2250c"
+    sha256 cellar: :any_skip_relocation, ventura:        "9003e906b745968d39b0757e13abc0cfb5d56bf93a77c251d295984a435016fe"
+    sha256 cellar: :any_skip_relocation, monterey:       "4f2fef33754a195db5e6d3a2738b8e1ba66f6ab1b873a9a983b7dca5dd03b9e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "68c64f352354f9e8f2c32f071ed632c57d9cdb27ad28db79eb825f5afedf90e4"
   end
 
   depends_on "go" => :build
